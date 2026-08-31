b05905ced31f275e79fa003cc40693e6c4db3a8f
xlilnyx <mas.kholil2005@gmail.com>
2026-08-31 14:21:16 +0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,11 @@
+const PASSWORD = "XLILNYX";
+
+function login() {
+    const input = document.getElementById("password").value;
+
+    if (input === PASSWORD) {
+        alert("THANKYOU FOR USING MY SCRIPT - COTANCE STORE");
+    } else {
+        alert("LU SIAPA MPRUY MAU PAKE SCRIPT TUAN XLILNYX 😂");
+    }
+}
